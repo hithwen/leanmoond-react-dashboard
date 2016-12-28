fa84611b7ed78b1d1e6016738cc7892e14b8e0c1
Julia S.Simon <hithwen@gmail.com>
2016-12-28 13:13:47 +0100

    simplify DashboardComponent

--- src/dashboard.tsx
+++ src/dashboard.tsx
@@ -4,20 +4,16 @@ import { DashboardItemComponent } from './dashboard-item';
 export const DashboardComponent = (props: {
   title: string,
   items : Array<[string, string]>}) => {
-  var dashboadItems = [];
-  for (var i=0; i < props.items.length; i++) {
-      dashboadItems.push(<DashboardItemComponent
-        icon={props.items[i][0]}
-        text={props.items[i][1]}
-        columnsize={12/props.items.length} />);
-  }
 
   return (
     <div className="dashboard">
       <div className="col-md-12 dashboardtitle">
           <h3 className="text-center">{props.title}</h3>
       </div>
-      {dashboadItems}
+        { props.items.map(
+          function(item) {
+            return <DashboardItemComponent icon={item[0]} text={item[1]} columnsize={4}/>
+        })}
     </div>
   );
 }
